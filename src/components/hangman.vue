de38158228791895da0e59961fd62b923af34ebf
Torin Berg <beau.berghausen@education.nsw.gov.au>
2018-12-11 16:16:08 +1100

    working hangman

--- src/components/hangman.vue
+++ src/components/hangman.vue
@@ -1,17 +1,158 @@
 <template>
   <div class="hangman" v-bind:style="{ backgroundImage: background }">
-
+    <div id="container">
+        <h1>Hangman</h1>
+        <div id="image-container">
+          <img src="http://www.johnmh.com/food/hang/hangmanA.gif" alt="">
+        </div>
+        <div id="word-container">
+          <h2 id="word"> {{ displayWord }} </h2>
+        </div>
+        <div id="keys-container">
+          <button :disabled="tries.indexOf('a') >= 0" @click="addChar('A')" class="key">A</button>
+          <button :disabled="tries.indexOf('b') >= 0" @click="addChar('B')" class="key">B</button>
+          <button :disabled="tries.indexOf('c') >= 0" @click="addChar('C')" class="key">C</button>
+          <button :disabled="tries.indexOf('d') >= 0" @click="addChar('D')" class="key">D</button>
+          <button :disabled="tries.indexOf('e') >= 0" @click="addChar('E')" class="key">E</button>
+          <button :disabled="tries.indexOf('f') >= 0" @click="addChar('F')" class="key">F</button>
+          <button :disabled="tries.indexOf('g') >= 0" @click="addChar('G')" class="key">G</button>
+          <button :disabled="tries.indexOf('h') >= 0" @click="addChar('H')" class="key">H</button>
+          <button :disabled="tries.indexOf('i') >= 0" @click="addChar('I')" class="key">I</button>
+          <button :disabled="tries.indexOf('j') >= 0" @click="addChar('J')" class="key">J</button>
+          <button :disabled="tries.indexOf('k') >= 0" @click="addChar('K')" class="key">K</button>
+          <button :disabled="tries.indexOf('l') >= 0" @click="addChar('L')" class="key">L</button>
+          <button :disabled="tries.indexOf('m') >= 0" @click="addChar('M')" class="key">M</button>
+          <button :disabled="tries.indexOf('n') >= 0" @click="addChar('N')" class="key">N</button>
+          <button :disabled="tries.indexOf('o') >= 0" @click="addChar('O')" class="key">O</button>
+          <button :disabled="tries.indexOf('p') >= 0" @click="addChar('P')" class="key">P</button>
+          <button :disabled="tries.indexOf('q') >= 0" @click="addChar('Q')" class="key">Q</button>
+          <button :disabled="tries.indexOf('r') >= 0" @click="addChar('R')" class="key">R</button>
+          <button :disabled="tries.indexOf('s') >= 0" @click="addChar('S')" class="key">S</button>
+          <button :disabled="tries.indexOf('t') >= 0" @click="addChar('T')" class="key">T</button>
+          <button :disabled="tries.indexOf('u') >= 0" @click="addChar('U')" class="key">U</button>
+          <button :disabled="tries.indexOf('v') >= 0" @click="addChar('V')" class="key">V</button>
+          <button :disabled="tries.indexOf('w') >= 0" @click="addChar('W')" class="key">W</button>
+          <button :disabled="tries.indexOf('x') >= 0" @click="addChar('X')" class="key">X</button>
+          <button :disabled="tries.indexOf('y') >= 0" @click="addChar('Y')" class="key">Y</button>
+          <button :disabled="tries.indexOf('z') >= 0" @click="addChar('Z')" class="key">Z</button>
+        </div>
+        <div id="reset-container">
+          <button @click="reset()">RESET</button>
+        </div>
+    </div>
   </div>
 </template>
 
 <script>
 
+
+var possibleWord = ['ability',
+'able',
+'about',
+'above',
+'accept',
+'according',
+'account',
+'across',
+'act',
+'action',
+'activity',
+'actually',
+'add',
+'address',
+'administration',
+'admit',
+'adult',
+'affect',
+'after']
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function displayInput(word, inputs) {
+  var string = "";
+  for(var i = 0; i < word.length; i++) { string += "_"; }
+
+  var inWord = word;
+  for(var n = 0; n < inputs.length; n++) {
+    var rex = new RegExp(String(inputs[n]), "g");
+    inWord = inWord.replace(rex, "1")
+  }
+
+  for(var k = 0; k < inWord.length; k++) {
+    if(inWord[k] == '1') {
+      var arr = string.split("");
+          arr[k] = word[k];
+          string = arr.join("");
+    }
+  }
+
+  return string.split("").join(" ");
+}
+
+function isCorrect(string, token) {
+  for(var i = 0; i < string.length; i++) {
+    if(string[i] == token) { return true; }
+  }
+  return false;
+}
+
+function isWin(string, tokens) {
+  for(var i = 0; i < string.length; i++) {
+    if(tokens.indexOf(string[i]) == -1) { return false; }
+  }
+  return true;
+}
+
 export default {
-  name: 'hangman',
+  name: "hangman",
   data() {
     return {
-      background: `linear-gradient(to bottom left, rgb(${100}, 0, ${140}), rgb(${100}, 0, ${20}))`,
+      lives: 6,
+      fullWord: "accept",
+      charInputs: [],
+      tries: [],
+      displayWord: "",
+      background: "linear-gradient(to bottom left, rgb(0, 0, 140), rgb(0, 0, 20))"
     }
+  },
+  methods: {
+    addChar(inChar) {
+      inChar = inChar.toLowerCase();
+      this.tries.push(inChar);
+      var win = isCorrect(this.fullWord, inChar);
+      if(win) {
+        this.charInputs.push(inChar);
+        this.displayWord = displayInput(this.fullWord, this.charInputs);
+        if(isWin(this.fullWord, this.charInputs)) {
+          alert("You win!")
+        }
+      } else {
+        this.lives--;
+        if(this.lives == 0) {
+          alert("You loose!");
+          alert(this.fullWord);
+        }
+      }
+    },
+    reset() {
+      var string = "";
+      this.lives = 6;
+      this.charInputs = [];
+      this.tries = [];
+      this.displayWord = "";
+
+      this.fullWord = possibleWord[getRandomInt(possibleWord.length)];
+      for(var i = 0; i < this.fullWord.length; i++) { string += "_"; }
+      this.displayWord = string.split("").join(" ");
+    }
+  },
+  created() {
+    var string = "";
+    this.fullWord = possibleWord[getRandomInt(possibleWord.length)];
+    for(var i = 0; i < this.fullWord.length; i++) { string += "_"; }
+    this.displayWord = string.split("").join(" ");
   }
 }
 
@@ -23,6 +164,81 @@ export default {
 .hangman {
   width: 100vw;
   height: 90vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
+
+#container {
+  width: 80vw;
+  height: 80vh;
+  background: white;
+  border-radius: 3px;
+}
+
+h1 {
+  text-align: center;
+  font-size: 5vh;
+  margin: 10px;
+}
+
+#image-container {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+
+#keys-container {
+  margin-top: 10px;
+  width: 80%;
+  padding-left: 10%;
+  padding-right: 10%;
+  display: flex;
+  justify-content: center;
+}
+
+#reset-container {
+  margin-top: 10px;
+  width: 80%;
+  padding-left: 10%;
+  padding-right: 10%;
+  display: flex;
+  justify-content: center;
+}
+
+#reset-container button {
+  width: 3vw;
+  height: 1.5vw;
+  border-radius: 2px;
+  margin: 5px;
+  background: black;
+  color: white;
+}
+
+#reset-container button:hover {
+  background: grey;
+}
+
+.key {
+  width: 2vw;
+  height: 1.5vw;
+  border-radius: 2px;
+  margin: 5px;
+  background: #e0e0e0;
+}
+
+.key:hover {
+  background: grey;
+}
+
+#word {
+  text-align: center;
+}
+
+.key:disabled {
+  background: black;
+  color: white;
+}
+
 
 </style>
