af0c40ceea37401dc77f07455b1791065dd0a8c7
Torin Berg <beau.berghausen@education.nsw.gov.au>
2018-12-12 17:21:30 +1100

    cleaned & commented

--- src/components/tempConverter.vue
+++ src/components/tempConverter.vue
@@ -1,19 +1,39 @@
 <template>
+  <!-- containing div of component w/dynamic background -->
   <div class="tempConverter" v-bind:style="{ backgroundImage: background }">
-    <div class="child">
-      <div class="flexChild">
+    <!-- container that contains all elements and has white background -->
+    <div class="content-container">
+      <!-- flexbox child for auto-centering -->
+      <div class="flex-child">
         <h1>Temperature Converter</h1>
 
+        <!-- auto-centering input container -->
         <div class="input-container">
           <span>Celcius:</span>
           <input class="input" id="input-c" type="number" v-model="celcius" @input="inputCelc" @focusOut="looseFocus" />
+          <!--
+            @id input-c
+            @type number
+            @vue-model celcius // reflects vue variable
+            @on-input inputCelc // calculate temperature and reflect it on farenheit input
+            @on-loose-focuss looseFocus // used to set inputs to 0 if empty
+          -->
         </div>
 
+        <!-- equals operator for good looks -->
         <p id="operator">=</p>
 
+        <!-- auto-centering input container -->
         <div class="input-container">
           <span>Fahrenheit:</span>
           <input class="input" id="input-f" type="number" v-model="farenheit" @input="inputFare" @focusOut="looseFocus" />
+          <!--
+            @id input-f
+            @type number
+            @vue-model farenheit // reflects vue variable
+            @on-input inputFare // calculate temperature and reflect it on celcius input
+            @on-loose-focuss looseFocus // used to set inputs to 0 if empty
+          -->
         </div>
       </div>
     </div>
@@ -22,43 +42,53 @@
 
 <script>
 
+// used to round outputs to 1 dp
 function round(n) {
   return Math.round((n + 0.00001) * 10) / 10;
 }
 
+// converts celcius input to farenheit output
 function convertToFahrenheit(celsius) {
   return round((celsius * 1.8) + 32);
 }
 
+// converts farenheit input to celcius output
 function convertToCelsius(fahrenheit) {
   return round((fahrenheit - 32) / 1.8);
 }
 
+// vue instance
 export default {
   name: 'tempConverter',
   data() {
-    return {
+    return { // set vue variables
       celcius: '0',
       farenheit: '0',
-      background: 'linear-gradient(to bottom left, rgb(120, 120, 120), rgb(100, 100, 120))'
+      background: 'linear-gradient(to bottom left, rgb(120, 120, 120), rgb(100, 100, 120))' // rgb dynamic background to reflect temp
     }
   },
   created() {
+    // calculate farenheight from 0 celcius
     this.farenheit = String(convertToFahrenheit(Number(this.celcius)));
-      this.background = `linear-gradient(to bottom left, rgb(${this.farenheit*2}, 0, ${140}), rgb(${this.farenheit*2}, 0, ${20}))`
+    // dynamicly update background to match temp
+    this.background = `linear-gradient(to bottom left, rgb(${this.farenheit*2}, 0, ${140}), rgb(${this.farenheit*2}, 0, ${20}))`
   },
   methods: {
-    inputCelc() {
+    inputCelc() { // on celcius input
+      // calculate farenheit from input celcius
       this.farenheit = String(convertToFahrenheit(Number(this.celcius)));
+      // dynamicly update background to match temp
       this.background = `linear-gradient(to bottom left, rgb(${this.farenheit*2}, 0, ${140}), rgb(${this.farenheit*2}, 0, ${20}))`
     },
-    inputFare() {
+    inputFare() { // on farenheit input
+      // calculate celcius from input farenheit
       this.celcius = String(convertToCelsius(Number(this.farenheit)));
+      // dynamicly update background to match temp
       this.background = `linear-gradient(to bottom left, rgb(${this.farenheit*2}, 0, ${140}), rgb(${this.farenheit*2}, 0, ${20}))`
     },
-    looseFocus() {
-      if(this.farenheit == '' || this.celcius == '') {
-        this.celcius = '0';
+    looseFocus() { // when one of inputs are deselected
+      if(this.farenheit == '' || this.celcius == '') { // check if either input is empty
+        this.celcius = '0'; // if so reset inputs
         this.farenheit = '0';
       }
     }
@@ -70,6 +100,8 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
+/* # containers # */
+
 .tempConverter {
   display: flex;
   align-items: center;
@@ -78,14 +110,14 @@ export default {
   height: 90vh;
 }
 
-.child {
+.content-container {
   width: 40vw;
   height: 60vh;
   background: white;
   border-radius: 5px;
 }
 
-.flexChild {
+.flex-child {
   display: flex;
   flex-direction: column;
   height: 100%;
@@ -93,16 +125,19 @@ export default {
   justify-content: space-evenly;
 }
 
+.input-container {
+  font-size: 4vh;
+  text-align: center;
+}
+
+/* # text # */
+
 h1 {
   text-align: center;
   font-size: 5vh;
   padding: 5vh;
 }
 
-.input-container {
-  font-size: 4vh;
-  text-align: center;
-}
 span {
   width: 50%;
 }
@@ -117,12 +152,5 @@ input {
 #operator {
   text-align: center;
   font-size: 4vh;
-}
-
-@media screen and (max-width: 600px) {
-  .child {
-    width: 60vw;
-   height: 80vh;
-  }
 }
 </style>
